7c97b620a9c540608775b5397b4fcd29da2fcba0
Josue <cuellarjosue3@gmail.com>
2022-09-30 04:11:13 -0700

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -28,10 +28,14 @@ app.get('/tip/:total/:tipPercentage' , (req, res) => {
     res.status(200).json({msg: `The tip is ${ans}`})
 })
 
-const arr = Math.random(["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now",
-"Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"])
-app.get('/magic', (req, res) => {
-    res.send(`<h1>${req.params.arr}</h1>`)
+const arr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now",
+"Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+app.get('/magic/:theQuestion', (req, res) => {
+    const randomNum = Math.floor(Math.random() * arr.length)
+
+    res.send(`<h1>${req.params.theQuestion}</h1>
+        <h2>${arr[randomNum]}</h2>
+    `)
 })
 
 /* END ROUTES */
