8a27f0db401a1f6c04ca954f80ffa089d1e116c2
Josue <cuellarjosue3@gmail.com>
2022-10-08 12:57:41 -0700

    Express hw done

--- server.js
+++ server.js
@@ -19,15 +19,16 @@ const app = express()
 
 // Mount Routes
 /*Start Routes */
+// Greeting routes
 app.get('/greeting/:name', (req, res) => {
     res.send(`<h1>${req.params.name}</h1>`)
 })
-
+// Tip route
 app.get('/tip/:total/:tipPercentage' , (req, res) => {
     const ans = parseInt(req.params.total) * parseInt(req.params.tipPercentage) / 100
     res.status(200).json({msg: `The tip is ${ans}`})
 })
-
+//8 Ball route
 const arr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now",
 "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 app.get('/magic/:theQuestion', (req, res) => {
